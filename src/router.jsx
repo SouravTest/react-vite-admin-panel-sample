458edf4b3e25c1910c3ffb488aa7e6262eab10a6
Souravkarmakar_Mayabious <souravk.mayabious@gmail.com>
2025-04-14 15:08:22 +0530

    add page not found

--- src/router.jsx
+++ src/router.jsx
@@ -7,21 +7,27 @@ import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import AdminLayout from "./layouts/AdminLayout";
-
+import NotFoundPage from "./pages/NotFoundPage";
 
 function RouteConfig() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="forgot" element={<Forgot />} />
-      <Route path="/dashboard" element={<AdminLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="users" element={<Users />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
-      
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="forgot" element={<Forgot />} />
+
+        {/* Nested routes under AdminLayout */}
+        <Route path="/dashboard" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+
+        {/* Catch-all route for unmatched routes */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
